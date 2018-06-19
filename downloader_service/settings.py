f85354a8b3187b04d4117d36f835f27921973c3c
joashivanmoodley <joash.i.moodley@gmail.com>
2018-06-19 23:58:10 +0200

    update db settings

--- downloader_service/settings.py
+++ downloader_service/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'downloader_service',
-        'USER': 'joash',
-        'PASSWORD': '',
+        'USER': 'downloader_service',
+        'PASSWORD': 'ZE6-euu-taQ-Zff',
         'HOST': 'localhost',
     }
 }
